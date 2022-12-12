bfd8acbe50ce02ac0785e1766fc9f518bea5bfdb
Kelvin Abaidoo <kabaidoo@hotmail.com>
2022-12-12 13:35:58 +0000

    Fixed controllers

--- src/main/java/io/codingtest/springboot/controllers/VerificationController.java
+++
@@ -1,4 +0,0 @@
-package io.codingtest.springboot.controllers;
-
-public class VerificationController {
-}
--- src/main/java/io/codingtest/springboot/models/User.java
+++ src/main/java/io/codingtest/springboot/models/User.java
@@ -1,7 +1,6 @@
 package io.codingtest.springboot.models;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
